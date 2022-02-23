fe09cd5feefa69526a4789afeb3556600b65605e
Lauracs13 <lauracs1213@gmail.com>
2022-02-23 03:23:03 +0300

    proyecto de la clase heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.static('public'));
 const path = require('path');
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor funcionando');
 });
 
